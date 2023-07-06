4592d5fbe3a28a57e50b47b53c4c6dfc01bf4f70
Ayah <84987424+ayah-uhhh@users.noreply.github.com>
2023-07-06 20:37:00 +0000

    jhujujhuhj

--- rpi-object-detection-master/rpi-object-detection-master/src/object-tracking-color/cv_object_tracking_color.py
+++ rpi-object-detection-master/rpi-object-detection-master/src/object-tracking-color/cv_object_tracking_color.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
             with open("./rgb_hardcode.csv", 'r') as file:
                 csvreader = csv.reader(file)
                 for row in csvreader:
-                    colors.append(tuple(row))
+                    colors.append(tuple(int(row)))
             print(colors)
         while True:
             # ----------------------------------------------------------------------
