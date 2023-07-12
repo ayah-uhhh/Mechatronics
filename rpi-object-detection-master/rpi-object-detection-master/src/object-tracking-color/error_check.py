98e3c7e7e56667fe4d441c5c04b91078110e5989
ayah <ayah.ayad333@gmail.com>
2023-07-12 14:22:38 -0500

    error check

---
+++ rpi-object-detection-master/rpi-object-detection-master/src/object-tracking-color/error_check.py
@@ -0,0 +1,28 @@
+# center 176, 126 of green
+# import the (cx,cy) being ran by cv_object_tracking_color.py 
+# check
+import numpy as np 
+from cv_object_tracking_color import *
+
+target_x = 176
+target_y = 126
+
+cam_sees = get_target()
+if cam_sees is not None:
+    cx, cy = cam_sees
+    print(f"Central position: ({cx}, {cy})")
+else:
+    print("Central position not found.")
+
+
+# Calculate the distance
+distance = np.sqrt((cx - target_x) ** 2 + (cy - target_y) ** 2)
+if distance != 0:
+    if cx < target_x:
+        print("move right") #bot motion
+    if cx > target_x:
+        print("move left")
+    if cy > target_y:
+        print("move down") # ramp motion
+    if cy < target_y:
+        print("move up") 
