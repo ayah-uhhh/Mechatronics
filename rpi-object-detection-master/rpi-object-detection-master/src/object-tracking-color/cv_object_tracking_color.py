7964eb1401e9226a7788eb53b056370744df35c4
jbaum314 <61604913+jbaum314@users.noreply.github.com>
2023-07-19 13:45:52 -0600

    Update cv_object_tracking_color.py

--- rpi-object-detection-master/rpi-object-detection-master/src/object-tracking-color/cv_object_tracking_color.py
+++ rpi-object-detection-master/rpi-object-detection-master/src/object-tracking-color/cv_object_tracking_color.py
@@ -57,7 +57,7 @@ cy = None
 
 def motor_cmd(arg):
     ser.reset_input_buffer()
-    ser.write(arg)
+    ser.write(bytes(arg))
     line = ser.readline().decode('utf-8').rstrip()
     print(line)
     time.sleep(1)
