1d23c89c663e95c256ec80730d950a92fa6daef8
stevenoid2 <91037982+stevenoid2@users.noreply.github.com>
2023-08-01 14:04:29 -0600

    Modified for Mega and added feedback

--- MotoronI2C/MotoronI2C.ino
+++ MotoronI2C/MotoronI2C.ino
@@ -1,19 +1,3 @@
-// This example shows a simple way to control multiple
-// Motoron Motor Controllers using I2C.
-//
-// The motors will stop but automatically recover if:
-// - Motor power (VIN) is interrupted, or
-// - A temporary motor fault occurs, or
-// - A command timeout occurs.
-//
-// The motors will stop until you power cycle or reset your
-// Arduino if:
-// - The Motoron experiences a reset.
-//
-// If a latched motor fault occurs, the motors
-// experiencing the fault will stop until you power cycle motor
-// power (VIN) or cause the motors to coast.
-
 #define FALSE (0)
 #define TRUE  (!FALSE)
 
@@ -29,15 +13,20 @@ const int BackCornerOuter  = 7;
 const int BackSideInner    = 8;
 const int BackSideOuter    = 9;
 
-#define ADC_STEPS 1024
-#define ADC_HYSTERESIS 200
+// Define the analog GPIO ports and associated constants
+#define ADC_STEPS        (1024)
+#define ADC_HYSTERESIS   (200)
 #define ADC_THRESHOLD_HI ((ADC_STEPS/2)+(ADC_HYSTERESIS/2))
 #define ADC_THRESHOLD_LO ((ADC_STEPS/2)-(ADC_HYSTERESIS/2))
 
-int AnalogButton1;  // Use the analog inputs for buttons...
+int AnalogButton1;   // Use the analog inputs for buttons...
 int AnalogButton2;
-int DigitalButton1; // ...and here are the digitized versions, which
-int DigitalButton2; // use analog hysterisis for debouncing
+int DigitalButton1;  // ...and here are the digitized versions, which
+int DigitalButton2;  //   use analog hysterisis for debouncing
+int PreviousButton1; // We save the previous state of the buttons so we
+int PreviousButton2; //   can act upon a change of state of the button
+int TriggerButton1;  // The "Trigger" bits here are set on the initial
+int TriggerButton2;  //   press of the corresponding button
 
 // Define the Motoron motor controller constants
 const int FrontControllerAddress = 15;
@@ -47,15 +36,11 @@ const int LeftMotor = 2;
 const int DefaultSpeed = 500;
 const int DefaultAcceleration = 100;
 const int DefaultDeceleration = 100;
-const int DefaultCommandTimeout = 200;
+const int DefaultCommandTimeout = 300;
 
 // This code creates an object for each Motoron controller.
 // The number passed as the first argument to each constructor
 // below should be the 7-bit I2C address of the controller.
-//
-// You should use the I2CSetAddresses sketch to
-// assign a unique I2C address to each Motoron, and then
-// modify the list below to match your setup.
 MotoronI2C FrontCtrl(FrontControllerAddress);
 MotoronI2C BackCtrl(BackControllerAddress);
 
@@ -82,7 +67,6 @@ void setupMotoron(MotoronI2C & mc) {
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-
   setupMotoron(FrontCtrl);
   setupMotoron(BackCtrl);
 
@@ -100,6 +84,20 @@ void setup() {
 
   BackCtrl.setMaxAcceleration(RightMotor, DefaultAcceleration);
   BackCtrl.setMaxDeceleration(RightMotor, DefaultDeceleration);
+/*
+*/
+  pinMode(FrontCornerInner, INPUT);           // set pin to input
+  pinMode(FrontCornerOuter, INPUT);           // set pin to input
+  pinMode(FrontSideInner  , INPUT);           // set pin to input
+  pinMode(FrontSideOuter  , INPUT);           // set pin to input
+  pinMode(BackCornerInner , INPUT);           // set pin to input
+  pinMode(BackCornerOuter , INPUT);           // set pin to input
+  pinMode(BackSideInner   , INPUT);           // set pin to input
+  pinMode(BackSideOuter   , INPUT);           // set pin to input
+
+  Serial.println("Hello World!!!2");
+  //Serial.write('Y');
+
 }
 
 void loop() {
@@ -107,10 +105,27 @@ void loop() {
 
   if (Serial.available()) {
     int SerialCommand = Serial.read();
-    Serial.write(SerialCommand);
+
+    //Serial.write(SerialCommand);
     switch (SerialCommand) {
-          /*
-  //case 'a': // Strafe left
+      //case 'd': // Backward
+      case 'r': // Backward
+        Serial.println("Ackr");
+        FrontCtrl.setSpeed(LeftMotor, -DefaultSpeed);
+        FrontCtrl.setSpeed(RightMotor, -DefaultSpeed);
+        BackCtrl.setSpeed(LeftMotor, -DefaultSpeed);
+        BackCtrl.setSpeed(RightMotor, -DefaultSpeed);
+      break;
+      //case 'f': // Forward
+      case 'l': // Forward
+        Serial.println("Ackl");
+        FrontCtrl.setSpeed(LeftMotor, DefaultSpeed);
+        FrontCtrl.setSpeed(RightMotor, DefaultSpeed);
+        BackCtrl.setSpeed(LeftMotor, DefaultSpeed);
+        BackCtrl.setSpeed(RightMotor, DefaultSpeed);
+      break;
+      /*
+      //case 'a': // Strafe left
       case 'l': // Strafe left
         FrontCtrl.setSpeed(LeftMotor, -DefaultSpeed);
         FrontCtrl.setSpeed(RightMotor, DefaultSpeed);
@@ -122,21 +137,6 @@ void loop() {
         FrontCtrl.setSpeed(LeftMotor, DefaultSpeed);
         FrontCtrl.setSpeed(RightMotor, -DefaultSpeed);
         BackCtrl.setSpeed(LeftMotor, -DefaultSpeed);
-        BackCtrl.setSpeed(RightMotor, DefaultSpeed);
-      break;
-      */
-      //case 'd': // Backward
-      case 'r': // Backward
-        FrontCtrl.setSpeed(LeftMotor, -DefaultSpeed);
-        FrontCtrl.setSpeed(RightMotor, -DefaultSpeed);
-        BackCtrl.setSpeed(LeftMotor, -DefaultSpeed);
-        BackCtrl.setSpeed(RightMotor, -DefaultSpeed);
-      break;
-      //case 'f': // Forward
-      case 'l': // Forward
-        FrontCtrl.setSpeed(LeftMotor, DefaultSpeed);
-        FrontCtrl.setSpeed(RightMotor, DefaultSpeed);
-        BackCtrl.setSpeed(LeftMotor, DefaultSpeed);
         BackCtrl.setSpeed(RightMotor, DefaultSpeed);
       break;
       /*
@@ -152,13 +152,13 @@ void loop() {
         BackCtrl.setSpeed(LeftMotor, DefaultSpeed);
         BackCtrl.setSpeed(RightMotor, 0);
       break;
-      */
       case 'q':
       break;
       case 'w':
       break;
       default:
       break;
+      */
     }
   }
 }
